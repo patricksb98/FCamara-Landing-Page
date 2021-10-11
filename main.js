20390136b4d5a80f310e4ef32d3184808444b325
Patrick Barbosa <patrick_souza98@hotmail.com>
2021-10-10 23:13:43 -0300

    Added back to top button

---
+++ main.js
@@ -0,0 +1,13 @@
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+window.addEventListener('scroll', function () {
+    backToTop()
+})
